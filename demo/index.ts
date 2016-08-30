da3ba63fc41c371773374795255de03654c1e629
Damián García <damian.garcia@zalando.de>
2016-08-30 17:15:30 +0200

    feature(demo): mock server calls to avoid actually hitting the backend

--- demo/index.ts
+++ demo/index.ts
@@ -1,15 +1,34 @@
 import 'angular';
 import 'angular-cookies';
+import 'angular-mocks/ngMockE2E';
 
 import 'solution-center-communicator';
 import '../node_modules/dress-code/dist/css/dress-code.min.css';
 
-angular.module('demo', ['solutioncenter.feedback'])
+angular.module('demo', ['solutioncenter.feedback', 'ngMockE2E'])
+  .run(DemoRunBlock)
   .controller('DemoController', DemoController);
+
+
+DemoRunBlock.$inject = ['$httpBackend'];
+
+function DemoRunBlock($httpBackend: ng.IHttpBackendService) {
+  $httpBackend.whenGET('https://ms-integration.norris.zalan.do/modules/3/feedback-status')
+    .respond(function() {
+    return [200, {
+      "feedbackAvailable": true
+    }];
+  });
+
+  $httpBackend.whenPOST('https://ms-integration.norris.zalan.do/modules/3/feedback')
+    .respond(function() {
+      return [200, {}];
+    });
+}
 
 function DemoController() {
   this.module = {
-    id: 1,
+    id: 3,
     name: 'Solution Center'
   };
 }
